eb032c6a65afef5ec978c516c061237a565c598e
AshikurRahman-sec <ashik129603@gmail.com>
2025-02-03 00:05:01 +0600

    add route in gateway main

--- gateway/main.py
+++ gateway/main.py
@@ -9,6 +9,8 @@ load_dotenv()
 from routers.auth_rout import router as auth_router
 from routers.frontpage_rout import router as frontpage_router
 from routers.services_rout import router as service_router
+from routers.userAndsettings_rout import router as user_settings_router
+from routers.paymentAndbooking_rout import router as payment_booking_router
 
 app = FastAPI()
 
@@ -27,3 +29,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(auth_router, prefix="/auth/gateway")
 app.include_router(frontpage_router, prefix="/api", tags=["Frontpage Data"])
 app.include_router(service_router, prefix="/service/gateway", tags=["Service"])
+app.include_router(user_settings_router, prefix="/service/gateway", tags=["User And Settings"])
+app.include_router(payment_booking_router, prefix="/service/gateway", tags=["Payment And Booking"])
