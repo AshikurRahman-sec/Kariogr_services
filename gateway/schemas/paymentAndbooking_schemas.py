d069884cce0fc5e18ebfe4c75f3ab4696b925113
AshikurRahman-sec <ashik129603@gmail.com>
2025-06-24 23:17:59 +0600

    add tools required in schemas

--- gateway/schemas/paymentAndbooking_schemas.py
+++ gateway/schemas/paymentAndbooking_schemas.py
@@ -64,6 +64,7 @@ class WorkerInfo(BaseModel):
     charge_amount: float
     discount: float
     charge_unit: ChargeUnit
+    tools_required: Optional[bool] = False
 
 class WorkerSelection(BaseModel):
     booking_id: str
